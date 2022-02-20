012a97196204427917283ed4407580d15fcb7213
Mrunal <80758264+msy1717@users.noreply.github.com>
2022-02-20 16:22:51 +0530

    Update start.py

--- PyroBot/plugins/start.py
+++ PyroBot/plugins/start.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton  
-from PyroBot.db.my import db,create_user,check_user,update_user,new_ref
+from PyroBot.db import *
 
 
 @Client.on_message(filters.command(["start"]))
