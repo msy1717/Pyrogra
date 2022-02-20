e02178f2cdcd65e3f15f99bdac5ad3869f998709
Mrunal <80758264+msy1717@users.noreply.github.com>
2022-02-20 16:27:48 +0530

    Update start.py

--- PyroBot/plugins/start.py
+++ PyroBot/plugins/start.py
@@ -1,6 +1,81 @@
 from pyrogram import Client, filters
 from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton  
 from PyroBot.db import *
+import redis
+
+REDIS_URI = 'redis-17358.c252.ap-southeast-1-1.ec2.cloud.redislabs.com:17358'
+REDIS_PASSWORD = '6nkq7KUJ2p1PYelzW30zXtgG6Afhu1XI'
+
+redis_info = REDIS_URI.split(':')
+dB = redis.StrictRedis(host=redis_info[0],port=redis_info[1],password=REDIS_PASSWORD,charset="utf-8",decode_responses=True)
+
+
+
+
+
+
+def create_user(userid,refby):
+    if refby!='n':
+        sql = 'Insert into users(userid,referred_by) values({},{})'.format(userid,refby)
+    else:
+        sql = 'Insert into users(userid) values({})'.format(userid)
+    db.execute(sql)
+    db.commit()
+
+def check_user(userid):
+    userid = str(userid)
+    sql = 'Select * from users where userid = {}'.format(userid)
+    a = db.execute(sql)
+    try:
+        r = a.fetchall()
+    except AttributeError:
+        r =None
+    if r:
+        return r[0]
+    else:
+        return False
+    
+
+def update_user(userid,balance,twitter_username,task_level,total_referrals):
+    sql = 'Update users SET '
+    a = ' Where userid = {}'.format(userid)
+    if balance != 'n':
+        if (twitter_username != 'n') or (task_level!='n') or (total_referrals != 'n'):
+            sql = sql + "balance = {}, ".format(balance)
+        else:
+            sql = sql + "balance = {}".format(balance)
+    if twitter_username != 'n':
+        if (task_level != 'n') or (total_referrals != 'n'):
+            sql = sql+"twitter_username = '{}', ".format(twitter_username)
+        else:
+            sql = sql+"twitter_username = '{}'".format(twitter_username)
+    if task_level != 'n':
+        if total_referrals != 'n':
+            sql = sql+'task_level = {}, '.format(task_level)
+        else:
+            sql = sql+'task_level = {}'.format(task_level)
+    if total_referrals != 'n':
+        sql = sql+'total_referrals = {}'.format(total_referrals)
+    sql = sql+a
+    db.execute(sql)
+    db.commit()
+
+def new_ref(refby,refto):
+    sql = "Insert into referral(refby,refto) values({},{})".format(str(refby),str(refto))
+    db.execute(sql)
+    db.commit()
+    user = check_user(refby)
+    total = user[6]
+    total = total+1
+    sql = "Update users Set total_referrals = {} where userid={}".format(total,refby)
+    db.execute(sql)
+    db.commit()
+    
+
+
+
+
+
 
 
 @Client.on_message(filters.command(["start"]))
