7346eb004f0fe17c61cce607e4351019affc5be1
Mrunal <80758264+msy1717@users.noreply.github.com>
2022-02-20 13:49:23 +0530

    Update start.py

--- PyroBot/plugins/start.py
+++ PyroBot/plugins/start.py
@@ -6,9 +6,10 @@ from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyb
 async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
-        text="Hello there!",
-        reply_to_message_id=update.message_id
-    )
+        text="Hello there!", reply_markup=ReplyKeyboardMarkup( [ ["A", "B", "C", "D"],["E", "F", "G"],["H", "I"],["J"]], resize_keyboard=True ) )
+       
+        
+
 
 
  
